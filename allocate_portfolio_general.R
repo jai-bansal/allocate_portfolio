b8346d66c592b4707302c56bbee7c968a2c876b6
jai-bridg <jai@bridg.com>
2015-10-14 12:30:37 -0700

    corrected file uploaded
    uploads correct file (previous upload was incorrect)

---
+++ allocate_portfolio_general.R
@@ -0,0 +1,83 @@
+# This script computes the number of shares of each asset necessary to achieve a target portfolio allocation when adding additional funds:
+# This script requires the user to input desired assets, target portfolio allocation, and current shares owned:
+
+# PREP:
+
+  # Load needed packages:
+  library(rvest)
+  library(data.table)
+
+# DEFINE TARGET PORTFOLIO ALLOCATION:
+
+  # Specify all desired assets in target portfolio:
+  # MANUAL INPUT REQUIRED:
+  # Example: assets = c('AAPL', 'GOOG')
+  assets = c()
+  
+  # Specify desired percentage of total portfolio for each asset in 'assets':
+  # Order of percentages should correspond to the order of assets provided in 'assets':
+  # Note: sum of 'target_percentages' should be 1:
+  # MANUAL INPUT REQUIRED:
+  # Example: target_percentages = c(0.25, 0.75)
+  target_percentages = c()
+ 
+# CURRENT PORTFOLIO:
+# Provide current portfolio in terms of number of shares:
+# Order of shares should correspond to the order of assets provided in 'assets':
+# MANUAL INPUT REQUIRED:
+# Example: current_shares = c(5, 10)
+current_shares = c()
+  
+# CURRENT PRICES:
+# Obtain current prices of assets specified in 'CURRENT PORTFOLIO':
+# Results are stored in the vector 'current_prices':
+# This section uses web scraping, which I think is limited to some number of requests per day:
+
+  # Create empty (for now) 'current_prices' vector:
+  current_prices = c()
+  
+  # Scrape prices for assets in 'assets':
+  for (i in assets)
+    {
+  
+      # Identify page with relevant asset price:
+      # This script uses Yahoo Finance price quotes:
+      page = html(paste0('https://finance.yahoo.com/q?s=', i))
+      
+      # Scrape price:
+      price = as.numeric(html_text(html_node(page, paste0('#yfs_l84_', tolower(i)))))
+      current_prices = c(current_prices, price)
+
+  }
+  rm(i, page, price)
+  
+# COMPUTE CURRENT PORTFOLIO VALUE:
+  
+  # Create data table with portfolio information:
+  portfolio = data.table(assets = assets, target_percentages = target_percentages, current_shares = current_shares, current_prices = current_prices)
+  rm(assets, target_percentages, current_shares, current_prices)
+  
+  # Compute equity for each asset:
+  portfolio$equity = portfolio$current_shares * portfolio$current_prices
+  
+  # Compute portfolio current value:
+  current_value = sum(portfolio$equity)
+
+# COMPUTE NEW ALLOCATION OF SHARES AFTER ADDING ADDITIONAL FUNDS:
+    
+  # Specify amount to be added($):
+  # MANUAL INPUT REQUIRED:
+  # Example: to_add = 10000.00
+  to_add = 
+  
+  # Compute total dollar value of portfolio:
+  total_value = current_value + to_add
+    
+  # Compute how much equity, how many total shares, and how many additional shares should be invested in each asset
+  # (in 'portfolio$assets') according to the target allocation:
+  portfolio$ideal_equity = portfolio$target_percentages * total_value
+  portfolio$ideal_shares = floor(portfolio$ideal_equity / portfolio$current_prices)
+  portfolio$shares_to_buy = portfolio$ideal_shares - portfolio$current_shares
+  
+  # Compute money that will be leftover after buying 'portfolio$shares_to_buy':
+  leftover = to_add - sum(portfolio$current_prices * portfolio$shares_to_buy)
