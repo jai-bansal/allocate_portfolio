29840950e2b75f39e2450881c11ec4fe0a8ab81d
jai-bansal <jbans79@gmail.com>
2016-11-15 14:04:04 -0800

    add current and ideal percentages
    add columns in portfolio for the current portfolio percentage of each
    security and the portfolio percentage under the ideal case.
    
    also did some tidying up and reformatting

--- balance_portfolio_general.R
+++ balance_portfolio_general.R
@@ -1,43 +1,43 @@
 # This script computes the number of shares of each asset necessary to achieve 
-# a target portfolio allocation when adding additional funds:
+# a target portfolio allocation when adding additional funds.
 # This script requires the user to input desired assets, target portfolio allocation, 
-# current shares owned, and funds to be added to portfolio:
+# current shares owned, and funds to be added to portfolio.
 
-# PREP:
+# PREP --------------------------------------------------------------------
 
-  # Load needed packages:
+  # Load needed packages.
   library(quantmod)
   library(data.table)
 
-# DEFINE TARGET PORTFOLIO ALLOCATION:
+# DEFINE TARGET PORTFOLIO ALLOCATION --------------------------------------
 
-  # Specify all desired assets in target portfolio:
-  # MANUAL INPUT REQUIRED:
+  # Specify all desired assets in target portfolio.
+  # MANUAL INPUT REQUIRED.
   # Example: assets = c('AAPL', 'GOOG')
   assets = c()
   
-  # Specify desired percentage of total portfolio for each asset in 'assets':
-  # Order of percentages should correspond to the order of assets provided in 'assets':
-  # Note: sum of 'target_percentages' should be 1:
-  # MANUAL INPUT REQUIRED:
+  # Specify desired percentage of total portfolio for each asset in 'assets'.
+  # Order of percentages should correspond to the order of assets provided in 'assets'.
+  # Note: sum of 'target_percentages' should be 1.
+  # MANUAL INPUT REQUIRED.
   # Example: target_percentages = c(0.25, 0.75)
   target_percentages = c()
- 
-# CURRENT PORTFOLIO:
-# Provide current portfolio in terms of number of shares:
-# Order of shares should correspond to the order of assets provided in 'assets':
-# MANUAL INPUT REQUIRED:
+
+# CURRENT PORTFOLIO -------------------------------------------------------
+# Provide current portfolio in terms of number of shares.
+# Order of shares should correspond to the order of assets provided in 'assets'.
+# MANUAL INPUT REQUIRED
 # Example: current_shares = c(5, 10)
 current_shares = c()
-  
-# CURRENT PRICES:
-# Obtain current prices of assets specified in 'CURRENT PORTFOLIO':
-# Results are stored in the vector 'current_prices':
 
-  # Create empty (for now) 'current_prices' vector:
+# CURRENT PRICES ----------------------------------------------------------
+# Obtain current prices of assets specified in 'CURRENT PORTFOLIO'.
+# Results are stored in the vector 'current_prices'.
+
+  # Create empty (for now) 'current_prices' vector.
   current_prices = c()
   
-  # Get prices for assets in 'assets':
+  # Get prices for assets in 'assets'.
   for (i in assets)
     {
   
@@ -49,35 +49,39 @@ current_shares = c()
 
   }
   rm(i, price)
-  
-# COMPUTE CURRENT PORTFOLIO VALUE:
-  
-  # Create data table with portfolio information:
+
+# COMPUTE CURRENT PORTFOLIO VALUE -----------------------------------------
+
+  # Create data table with portfolio information.
   portfolio = data.table(assets = assets, target_percentages = target_percentages, 
                          current_shares = current_shares, current_prices = current_prices)
   rm(assets, target_percentages, current_shares, current_prices)
   
-  # Compute equity for each asset:
-  portfolio$equity = portfolio$current_shares * portfolio$current_prices
+  # Compute equity for each asset.
+  portfolio$current_equity = portfolio$current_shares * portfolio$current_prices
   
-  # Compute portfolio current value:
-  current_value = sum(portfolio$equity)
+  # Compute portfolio current value.
+  current_value = sum(portfolio$current_equity)
 
-# COMPUTE NEW ALLOCATION OF SHARES AFTER ADDING ADDITIONAL FUNDS:
-    
-  # Specify amount to be added($):
-  # MANUAL INPUT REQUIRED:
+# COMPUTE NEW ALLOCATION OF SHARES AFTER ADDING ADDITIONAL FUNDS ----------
+
+  # Specify amount to be added($).
+  # MANUAL INPUT REQUIRED
   # Example: to_add = 10000.00
   to_add = 
   
-  # Compute total dollar value of portfolio:
+  # Compute total dollar value of portfolio.
   total_value = current_value + to_add
     
   # Compute how much equity, how many total shares, and how many additional shares should be invested in each asset
-  # (in 'portfolio$assets') according to the target allocation:
+  # (in 'portfolio$assets') according to the target allocation.
   portfolio$ideal_equity = portfolio$target_percentages * total_value
   portfolio$ideal_shares = floor(portfolio$ideal_equity / portfolio$current_prices)
   portfolio$shares_to_buy = portfolio$ideal_shares - portfolio$current_shares
   
-  # Compute money that will be leftover after buying 'portfolio$shares_to_buy':
+  # Compute the percentage allocation of portfolio at the current time and after buying the shares in 'portfolio$shares_to_buy'.
+  portfolio$current_percentage = round(100 * portfolio$current_equity / sum(portfolio$current_equity), 2)
+  portfolio$ideal_percentage = round(100 * portfolio$ideal_equity / sum(portfolio$ideal_equity), 2)
+  
+  # Compute money that will be leftover after buying 'portfolio$shares_to_buy'.
   leftover = to_add - sum(portfolio$current_prices * portfolio$shares_to_buy)
